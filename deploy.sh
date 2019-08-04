ee5f29fc7a51efc9575e46e6b982fdc49829a5f1
Mike Trout <mike.trout@gmail.com>
2019-08-04 19:10:53 +0100

    Use Travis to automatically deploy to GKE.

---
+++ deploy.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Install gcloud
+curl https://sdk.cloud.google.com | bash > /dev/null
+
+# Promote gcloud to PATH top priority (prevent using old version from Travis)
+source $HOME/google-cloud-sdk/path.bash.inc
+
+# Make sure kubectl is updated to latest version
+gcloud components update kubectl
+
+gcloud auth activate-service-account --key-file gcloud-service-account-secret.json
+gcloud container clusters get-credentials gke-cluster --zone us-central1-a --project www.miketrout.dev
+kubectl apply -f k8s.yaml

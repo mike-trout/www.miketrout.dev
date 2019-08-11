fe5e92733231cd13177a0dd3f0a1cc3471d28a42
Mike Trout <mike.trout@gmail.com>
2019-08-11 20:26:42 +0100

    Iterate over experience array. Set request header after open.

--- miketrout.dev/script/main.js
+++ miketrout.dev/script/main.js
@@ -2,13 +2,13 @@ function reqListener() {
   console.log(this.responseText);
   var experience = JSON.parse(this.responseText);
   var experienceItemsJobRole = document.getElementsByClassName('experience-item__job-role');
-  for (var i = 0; i < experienceItemsJobRole.length; i++) {
+  for (var i = 0; i < experience.length; i++) {
     experienceItemsJobRole[i].innerHTML = experience[i].jobRole;
   }
 }
 
 var oReq = new XMLHttpRequest();
 oReq.addEventListener('load', reqListener);
-oReq.setRequestHeader('Accept', 'application/json');
 oReq.open('GET', '/api/experience');
+oReq.setRequestHeader('Accept', 'application/json');
 oReq.send();

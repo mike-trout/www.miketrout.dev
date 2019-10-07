e0f02f0431a95326f30489c50ff8dcbb31aee8a9
Mike Trout <mike.trout@gmail.com>
2019-10-07 15:42:27 +0100

    Huge refactoring.

--- miketrout.dev/script/main.js
+++ miketrout.dev/script/main.js
@@ -1,9 +1,10 @@
 'use strict';
 
 // Create the placeholders for the experience items
+// count: Number of placeholders to create
 function createExperienceItemPlaceholders(count) {
   // Get the experience container div
-  var experienceDiv = document.getElementsByClassName('experience')[0];
+  var experienceDiv = document.getElementById('experience');
   // Create the number of placeholders specified by count
   for (var i = 0; i < count; i++) {
     // Create the experience item div
@@ -49,185 +50,187 @@ function createExperienceItemPlaceholders(count) {
   }
 }
 
-// Get experience resource from the specified URL
-function getExperience(url) {
+// Create the placeholders for the projects items
+// count: Number of placeholders to create
+function createProjectsItemPlaceholders(count) {
+  // Get the projects container div
+  var projectsDiv = document.getElementById('projects');
+  // Create the number of placeholders specified by count
+  for (var i = 0; i < count; i++) {
+    // Create the projects item div
+    var projectsItemDiv = document.createElement('div');
+    projectsItemDiv.classList.add(
+      'projects__item',
+      'projects__item--placeholder'
+    );
+    // Create the projects item name div and append to the
+    // projects item div
+    var projectsItemNameDiv = document.createElement('div');
+    projectsItemNameDiv.classList.add(
+      'projects__item-name',
+      'projects__item-name--placeholder'
+    );
+    projectsItemDiv.appendChild(projectsItemNameDiv);
+    // Create the projects item description div and append to
+    // the projects item div
+    var projectsItemDescriptionDiv = document.createElement('div');
+    projectsItemDescriptionDiv.classList.add(
+      'projects__item-description',
+      'projects__item-description--placeholder'
+    );
+    projectsItemDiv.appendChild(projectsItemDescriptionDiv);
+    // Add the projects item div to the projects container div
+    projectsDiv.appendChild(projectsItemDiv);
+  }
+}
+
+// Create the placeholders for the skills items
+// count: Number of placeholders to create
+function createSkillsItemPlaceholders(count) {
+  // Get the skills detail container div
+  var skillsDetailDiv = document.getElementById('skills-detail');
+  // Create the number of placeholders specified by count
+  for (var i = 0; i < count; i++) {
+    // Create the skills detail item div
+    var skillsDetailItemDiv = document.createElement('div');
+    skillsDetailItemDiv.classList.add('skills__detail-item');
+    // Create the skills detail item name div and append to the
+    // skills detail item div
+    var skillsDetailItemNameDiv = document.createElement('div');
+    skillsDetailItemNameDiv.classList.add(
+      'skills__detail-item-name',
+      'skills__detail-item-name--placeholder'
+    );
+    skillsDetailItemDiv.appendChild(skillsDetailItemNameDiv);
+    // Create the skills detail item score div and append to the
+    // skills detail item div
+    var skillsDetailItemScoreDiv = document.createElement('div');
+    skillsDetailItemScoreDiv.classList.add('skills__detail-item-score');
+    skillsDetailItemDiv.appendChild(skillsDetailItemScoreDiv);
+    // Create the skills detail item score bar div and append to
+    // the skills detail item score div
+    var skillsDetailItemScoreBarDiv = document.createElement('div');
+    skillsDetailItemScoreBarDiv.classList.add(
+      'skills__detail-item-score-bar',
+      'skills__detail-item-score-bar--placeholder'
+    );
+    skillsDetailItemScoreDiv.appendChild(skillsDetailItemScoreBarDiv);
+    // Append the skills detail item div to the skills detail
+    // container
+    skillsDetailDiv.appendChild(skillsDetailItemDiv);
+  }
+}
+
+// Callback function to display the experience resource collection
+// experience: The experience resource collection
+function displayExperienceCallback(experience) {
+  // Create placeholders, accounting for the existing placeholder
+  createExperienceItemPlaceholders(experience.count - 1);
+  // Get the experience resources with an artifical delay to emphasise
+  // that the resources are being fetched from a web service
+  setTimeout(function () {
+    for (var i = 0; i < parseInt(experience.count); i++) {
+      getResource(experience.items[i], displayExperienceItemCallback, { index: i });
+    }
+  }, 250);
+}
+
+// Callback function to display an experience item
+// experienceItem: The experience item to display
+// params: Parameters object containing index to display
+function displayExperienceItemCallback(experienceItem, params) {
+  var index = params.index;
+  var experienceItemDiv = document.getElementsByClassName('experience__item')[index];
+  var experienceItemJobRoleDiv = document.getElementsByClassName('experience__item-job-role')[index];
+  experienceItemJobRoleDiv.innerHTML = experienceItem.jobRole;
+  experienceItemJobRoleDiv.classList.remove('experience__item-job-role--placeholder');
+  var experienceItemDateRangeDiv = document.getElementsByClassName('experience__item-date-range')[index];
+  experienceItemDateRangeDiv.innerHTML = experienceItem.dateRange;
+  experienceItemDateRangeDiv.classList.remove('experience__item-date-range--placeholder');
+  var experienceItemEmployerDiv = document.getElementsByClassName('experience__item-employer')[index];
+  experienceItemEmployerDiv.innerHTML = experienceItem.employer;
+  experienceItemEmployerDiv.classList.remove('experience__item-employer--placeholder');
+  var experienceItemDescriptionDiv = document.getElementsByClassName('experience__item-description')[index];
+  experienceItemDescriptionDiv.innerHTML = experienceItem.description;
+  experienceItemDescriptionDiv.classList.remove('experience__item-description--placeholder');
+  experienceItemDiv.classList.remove('experience__item--placeholder');
+}
+
+// Callback function to display the projects resource collection
+// projects: The projects resource collection
+function displayProjectsCallback(projects) {
+  // Create placeholders, accounting for the existing placeholder
+  createProjectsItemPlaceholders(projects.length - 1);
+  // Display the project resources with an artifical delay to emphasise that the
+  // resources are being fetched from an API
+  setTimeout(function () {
+    for (var i = 0; i < projects.length; i++) {
+      var projectsItemNameDiv = document.getElementsByClassName('projects__item-name')[i];
+      projectsItemNameDiv.innerHTML = projects[i].name;
+      projectsItemNameDiv.classList.remove('projects__item-name--placeholder');
+      var projectsItemDescriptionDiv = document.getElementsByClassName('projects__item-description')[i];
+      projectsItemDescriptionDiv.innerHTML = projects[i].description;
+      projectsItemDescriptionDiv.classList.remove('projects__item-description--placeholder');
+      var projectsItemDiv = document.getElementsByClassName('projects__item')[i];
+      projectsItemDiv.classList.remove('projects__item--placeholder');
+    }
+  }, 250);
+}
+
+// Callback function to display the skills resource collection
+// skills: The skills resource collection
+function displaySkillsCallback(skills) {
+  // Create placeholders, accounting for the existing 3 placeholders
+  createSkillsItemPlaceholders(skills.length - 3);
+  // Display the skills resources with an artifical delay to emphasise that the
+  // resources are being fetched from a web service
+  setTimeout(function () {
+    for (var i = 0; i < skills.length; i++) {
+      var skillsDetailItemNameDiv = document.getElementsByClassName('skills__detail-item-name')[i];
+      skillsDetailItemNameDiv.innerHTML = skills[i].name;
+      skillsDetailItemNameDiv.classList.remove('skills__detail-item-name--placeholder');
+      var skillsDetailItemScoreBarDiv = document.getElementsByClassName('skills__detail-item-score-bar')[i];
+      skillsDetailItemScoreBarDiv.style.width = (parseInt(skills[i].level) * 10) + '%';
+      skillsDetailItemScoreBarDiv.classList.remove('skills__detail-item-score-bar--placeholder');
+    }
+    var skillsDetailDiv = document.getElementById('skills-detail');
+    skillsDetailDiv.classList.remove('skills__detail--placeholder');
+  }, 250);
+}
+
+// Get resource
+// url: The URL to get the resource from
+// displayResourceCallback: function to display the resource
+// params: optional additional paramters to pass to the callback
+function getResource(url, displayResourceCallback, params) {
   var xhr = new XMLHttpRequest();
   xhr.open('GET', url, true);
+  xhr.setRequestHeader('Accept', 'application/json');
+  xhr.timeout = 2000;
   xhr.onload = function () {
     if (xhr.readyState === 4) {
       if (xhr.status === 200) {
-        var experience = JSON.parse(xhr.responseText);
-        createExperienceItemPlaceholders(experience.count);
-        for (var i = 0; i < parseInt(experience.count); i++) {
-          getExperienceItem(experience.items[i], i);
-        }
+        var resource = JSON.parse(xhr.responseText);
+        displayResourceCallback(resource, params);
       } else {
-        console.error(xhr.statusText);
+        console.error(`Error getting resource ${url}\n${xhr.statusText}`);
       }
     }
-  };
+  }
   xhr.onerror = function (e) {
-    console.error(e);
-  };
-  xhr.ontimeout = function () {
-    console.error('The request to the experience service timed out.');
-  };
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.timeout = 2000;
+    console.error(`Error getting resource ${url}\n${e}`);
+  }
+  xhr.timeout = function () {
+    console.error(`Timed out getting resource ${url}`)
+  }
   xhr.send();
-}
-
-function getExperienceItem(url, index) {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', url, true);
-  xhr.onload = function () {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        var experienceItem = JSON.parse(xhr.responseText);
-        displayExperienceItem(experienceItem, index);
-      } else {
-        console.error(xhr.statusText);
-      }
-    }
-  };
-  xhr.onerror = function (e) {
-    console.error(e);
-  };
-  console.ontimeout = function () {
-    console.error('The request to the experience service timed out.');
-  };
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.timeout = 2000;
-  xhr.send();
-}
-
-function getProjects() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.miketrout.dev/projects/', true);
-  xhr.onload = function (e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        displayProjects(JSON.parse(xhr.responseText));
-      } else {
-        console.error(xhr.statusText);
-      }
-    }
-  };
-  xhr.onerror = function (e) {
-    console.error(xhr.statusText);
-  };
-  xhr.ontimeout = function () {
-    console.error('The request to the projects service timed out.');
-  };
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.timeout = 2000;
-  xhr.send();
-}
-
-function getSkills() {
-  var xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.miketrout.dev/skills/', true);
-  xhr.onload = function (e) {
-    if (xhr.readyState === 4) {
-      if (xhr.status === 200) {
-        displaySkills(JSON.parse(xhr.responseText));
-      } else {
-        console.error(xhr.statusText);
-      }
-    }
-  };
-  xhr.onerror = function (e) {
-    console.error(xhr.statusText);
-  }
-  xhr.ontimeout = function () {
-    console.error('The request to the skills service timed out.');
-  }
-  xhr.setRequestHeader('Accept', 'application/json');
-  xhr.timeout = 2000;
-  xhr.send();
-}
-
-function displayExperienceItem(experienceItem, index) {
-  var experienceItemContainer = document.getElementsByClassName('experience__item')[index];
-  var experienceItemJobRole = document.getElementsByClassName('experience__item-job-role')[index];
-  experienceItemJobRole.innerHTML = experienceItem.jobRole;
-  experienceItemJobRole.classList.remove('experience__item-job-role--placeholder');
-  var experienceItemDateRange = document.getElementsByClassName('experience__item-date-range')[index];
-  experienceItemDateRange.innerHTML = experienceItem.dateRange;
-  experienceItemDateRange.classList.remove('experience__item-date-range--placeholder');
-  var experienceItemEmployer = document.getElementsByClassName('experience__item-employer')[index];
-  experienceItemEmployer.innerHTML = experienceItem.employer;
-  experienceItemEmployer.classList.remove('experience__item-employer--placeholder');
-  var experienceItemDescription = document.getElementsByClassName('experience__item-description')[index];
-  experienceItemDescription.innerHTML = experienceItem.description;
-  experienceItemDescription.classList.remove('experience__item-description--placeholder');
-  experienceItemContainer.classList.remove('experience__item--placeholder');
-}
-
-function displayProjects(projects) {
-  var projectsItemPlaceholders = document.getElementsByClassName('projects__item--placeholder');
-  var numPlaceholders = projectsItemPlaceholders.length;
-  var numProjects = projects.length;
-  if (numPlaceholders > numProjects) {
-    for (var i = 0; i < numPlaceholders - numProjects; i++) {
-      projectsItemPlaceholders[0].parentElement.removeChild(projectsItemPlaceholders[0]);
-    }
-  } else if (numPlaceholders < numProjects) {
-    for (var i = 0; i < numProjects - numPlaceholders; i++) {
-      projectsItemPlaceholders[0].parentElement.appendChild(projectsItemPlaceholders[0].cloneNode(true));
-    }
-  }
-  var projectsItemNames = document.getElementsByClassName('projects__item-name');
-  var projectsItemDescriptions = document.getElementsByClassName('projects__item-description');
-  var projectsItems = document.getElementsByClassName('projects__item');
-  for (var i = 0; i < numProjects; i++) {
-    projectsItemNames[i].innerHTML = projects[i].name;
-    projectsItemDescriptions[i].innerHTML = projects[i].description;
-    projectsItemNames[i].classList.remove('projects__item-name--placeholder');
-    projectsItemDescriptions[i].classList.remove('projects__item-description--placeholder');
-    projectsItems[i].classList.remove('projects__item--placeholder');
-  }
-}
-
-function displaySkills(skills) {
-  var skillNamePlaceholders = document.getElementsByClassName('skills__detail-item-name--placeholder');
-  var numPlaceholders = skillNamePlaceholders.length;
-  var numSkills = skills.length;
-  if (numPlaceholders > numSkills) {
-    for (var i = 0; i < numPlaceholders - numSkills; i++) {
-      skillNamePlaceholders[0].parentElement.removeChild(skillNamePlaceholders[0]);
-    }
-  } else if (numPlaceholders < numSkills) {
-    for (var i = 0; i < numSkills - numPlaceholders; i++) {
-      skillNamePlaceholders[0].parentElement.appendChild(skillNamePlaceholders[0].cloneNode(true));
-    }
-  }
-  var skillLevelPlaceholders = document.getElementsByClassName('skills__detail-item-score--placeholder');
-  numPlaceholders = skillLevelPlaceholders.length;
-  if (numPlaceholders > numSkills) {
-    for (var i = 0; i < numPlaceholders - numSkills; i++) {
-      skillLevelPlaceholders[0].parentElement.removeChild(skillLevelPlaceholders[0]);
-    }
-  } else if (numPlaceholders < numSkills) {
-    for (var i = 0; i < numSkills - numPlaceholders; i++) {
-      skillLevelPlaceholders[0].parentElement.appendChild(skillLevelPlaceholders[0].cloneNode(true));
-    }
-  }
-  var skillNames = document.getElementsByClassName('skills__detail-item-name');
-  for (var i = 0; i < numSkills; i++) {
-    skillNames[i].innerHTML = skills[i].name;
-    skillNames[i].classList.remove('skills__detail-item-name--placeholder');
-  }
-  var skillLevels = document.getElementsByClassName('skills__detail-item-score');
-  for (var i = 0; i < numSkills; i++) {
-    skillLevels[i].style.width = skills[i].level + '0%';
-    skillLevels[i].classList.remove('skills__detail-item-score--placeholder');
-  }
-  document.getElementsByClassName('skills__detail--placeholder')[0].classList.remove('skills__detail--placeholder');
 }
 
 document.body.onload = function () {
-  getExperience('https://api.miketrout.dev/experience/');
-  getProjects();
-  getSkills();
-}
+  // Get the Experience resource
+  getResource('https://api.miketrout.dev/experience/', displayExperienceCallback);
+  // Get the Projects resource
+  getResource('https://api.miketrout.dev/projects/', displayProjectsCallback);
+  // Get the Skills resource
+  getResource('https://api.miketrout.dev/skills/', displaySkillsCallback);
+};
